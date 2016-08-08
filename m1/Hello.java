8d93c9da27c99689a2f71ed19aae6b4107944327
lipeng <woshipp07@sina.com>
2016-08-08 10:56:38 +0800

    add Demo.java File

--- m1/Hello.java
+++ m1/Hello.java
@@ -1,6 +1,7 @@
 public class Hello{
 	public static void main(String args[]){
 		System.out.println("Hello yootk");
-		System.out.println("www.yootk.comfff");
+		System.out.println("www.yootk.com");
+		System.out.println("www.yootk.com");
 	}
 }
